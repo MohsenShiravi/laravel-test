b37f770f60ad110a8e6ac183601fba3c38f3917b
mohsen sh <m.shiravi7770@gmail.com>
2022-07-07 17:53:18 +0430

    chang PostController for Relationships to tag

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,7 +37,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index',compact('posts'));
+        return view ('posts.index',compact('posts'));
     }
 
     public function edit(Post $post)
